ca075b67cca93607704865193ed1ded3428ecd60
Saeko Fukui <saekof@CCL107iM04.local>
2017-02-20 20:01:47 -0800

    update

--- StarBucks/Staff.h
+++ StarBucks/Staff.h
@@ -7,7 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Order.h"
+#import "Coffee.h"
 
 @interface Staff : NSObject
+
+@property (strong, nonatomic) NSString* name;
+@property (nonatomic) NSInteger perHourWage;
+@property (strong, nonatomic) NSArray<NSDate*> *workingHours;
+@property (strong, nonatomic) NSArray<NSDate*> *workingDays;
 
 @end
